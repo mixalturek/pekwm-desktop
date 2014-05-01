eac261a6862dc5545c0ea4c0921ca1345f6d6bef
Michal Turek <mixalturek@users.sf.net>
2014-05-01 16:42:49 +0200

    Terminals
    - xterm is default terminal application.
    - xterm and yeahconsole customization.
    - xterm and yeahconsole are executed after login.
    - Script for installation of debian packages.

---
+++ util/install_deb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# pekwm
+apt-get install pewkm
+
+# xset, xrdb
+apt-get install x11-xserver-utils
+
+# xterm
+apt-get install xterm
+
+# yeahconsole
+apt-get install yeahconsole
+
+# mc
+apt-get install mc
