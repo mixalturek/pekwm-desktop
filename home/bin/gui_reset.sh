83280134dadaeb46a9f837c9fd74388101d0d768
Michal Turek <mixalturek@users.sf.net>
2014-06-11 20:47:36 +0200

    Debian submenu, xsysguard
    - Debian submenu added, it is generated automatically for all installed software.
    - xsysguard_smallgray shortcut is executed.

--- home/bin/gui_reset.sh
+++ home/bin/gui_reset.sh
@@ -5,6 +5,6 @@ killall netwmpager 2> /dev/null
 killall xxkb 2> /dev/null
 killall stalonetray 2> /dev/null
 
-xsysguard smallgray/clock smallgray/cpu smallgray/load smallgray/mem smallgray/net_eth0 smallgray/net_wlan0 &
+xsysguard_smallgray temp &
 netwmpager &
 xxkb &
